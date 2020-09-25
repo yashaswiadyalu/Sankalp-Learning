915855b68f6067ff4e44edcaf5943617044c3313
Sumanth Kodiyalbail <SumanthKodiyalbail@gmail.com>
2020-09-25 20:08:01 +0530

    Day-01 solutions added

---
+++ Day-0.py
@@ -0,0 +1,5 @@
+# print the content saved in input_string
+
+input_string =i nput()   # user input
+print('Hello, World.')
+print(input_string) #printing the contents of input_string
---
+++ Day-1.py
@@ -0,0 +1,10 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a=int(input())
+b=float(input())
+c=input()
+print(a+i)
+print(float(b+d))
+print(s+c)	

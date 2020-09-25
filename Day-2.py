feb70b2d73ecd038392092dd378dce4e7c763d98
Sumanth Kodiyalbail <SumanthKodiyalbail@gmail.com>
2020-09-25 21:55:40 +0530

    Day-2 problem solution added

---
+++ Day-2.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost*(tip_percent/100)
+    tax = meal_cost*(tax_percent/100)
+    result=round(meal_cost+tip+tax)
+    print(result)
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+    tip_percent = int(input())
+    tax_percent = int(input())
+    solve(meal_cost, tip_percent, tax_percent)
